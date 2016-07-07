tu-darmstadt-ros-pkg/hector_localization

commit 81b976da6a54197357c2fa083d9c4e20e9121019

hector_pose_estimation_core: refactored vector and matrix classes
Instead of inheriting from Eigen::Matrix<> types, hector_pose_estimation now simply defines
typedefs for all kind of use cases. The former SymmetricMatrix_<> and SkewSymmetricMatrix classes
have been replaced by Eigen::MatrixBase extension (https://eigen.tuxfamily.org/dox-devel/TopicCustomizingEigen.html)
and by a free function that returns a skew-symmetric matrix.

The refactoring was required for the release in Ubuntu Xenial and Eigen 3.3. The previous code was
too fragile in case of Eigen upgrades and did not compile in Xenial due to changes in the Eigen::internal::traits<T>
interface.

hector_pose_estimation_core/include/hector_pose_estimation/input.h
@@ -55,8 +55,8 @@ class Input_ : public Input {
 public:
   enum { Dimension = _Dimension };
   typedef Input_<Dimension> Type;
-  typedef ColumnVector_<Dimension> Vector;
-  typedef SymmetricMatrix_<Dimension> Variance;
+  typedef typename ColumnVector_<Dimension>::type Vector;
+  typedef typename SymmetricMatrix_<Dimension>::type Variance;
 
   typedef boost::shared_ptr< Input_<_Dimension> > Ptr;
 
@@ -109,7 +109,7 @@ protected:
 
 namespace traits {
   template <class Model> struct Input {
-    enum { Dimension = 0 };
+    static const int Dimension = 0;
     typedef Input_<Dimension> Type;
     typedef ColumnVector_<0> Vector;
     typedef SymmetricMatrix_<0> Variance;

hector_pose_estimation_core/include/hector_pose_estimation/measurement_model.h
@@ -55,30 +55,30 @@ namespace traits {
 
   template <class Derived, int _Dimension = Derived::MeasurementDimension>
   struct MeasurementModel {
-    enum { MeasurementDimension = _Dimension };
-    typedef ColumnVector_<MeasurementDimension> MeasurementVector;
-    typedef SymmetricMatrix_<MeasurementDimension> NoiseVariance;
-    typedef Matrix_<MeasurementDimension,Dynamic> MeasurementMatrix;
-    typedef Matrix_<State::Covariance::RowsAtCompileTime,MeasurementDimension> GainMatrix;
-    typedef ColumnVector_<State::Covariance::RowsAtCompileTime> UpdateVector;
+    static const int MeasurementDimension = _Dimension;
+    typedef typename ColumnVector_<MeasurementDimension>::type MeasurementVector;
+    typedef typename SymmetricMatrix_<MeasurementDimension>::type NoiseVariance;
+    typedef typename Matrix_<MeasurementDimension,Dynamic>::type MeasurementMatrix;
+    typedef typename Matrix_<State::Covariance::RowsAtCompileTime,MeasurementDimension>::type GainMatrix;
+    typedef typename ColumnVector_<State::Covariance::RowsAtCompileTime>::type UpdateVector;
 
-    enum { InputDimension = traits::Input<Derived>::Dimension };
+    static const int InputDimension = traits::Input<Derived>::Dimension;
     typedef typename traits::Input<Derived>::Type InputType;
     typedef typename traits::Input<Derived>::Vector InputVector;
-    typedef Matrix_<MeasurementDimension,InputDimension> InputMatrix;
+    typedef typename Matrix_<MeasurementDimension,InputDimension>::type InputMatrix;
   };
 
   #define MEASUREMENT_MODEL_TRAIT(Derived, _Dimension) \
     typedef typename traits::MeasurementModel<Derived, _Dimension> trait; \
     \
-    enum { MeasurementDimension = _Dimension }; \
+    static const int MeasurementDimension = _Dimension; \
     typedef typename trait::MeasurementVector MeasurementVector; \
     typedef typename trait::NoiseVariance NoiseVariance; \
     typedef typename trait::MeasurementMatrix MeasurementMatrix; \
     typedef typename trait::GainMatrix GainMatrix; \
     typedef typename trait::UpdateVector UpdateVector; \
     \
-    enum { InputDimension = trait::InputDimension }; \
+    static const int InputDimension = trait::InputDimension; \
     typedef typename trait::InputType InputType; \
     typedef typename trait::InputVector InputVector; \
     typedef typename trait::InputMatrix InputMatrix; \

hector_pose_estimation_core/include/hector_pose_estimation/system_model.h
@@ -59,20 +59,20 @@ namespace traits {
 
   template <class Derived, int _VectorDimension = Derived::VectorDimension, int _CovarianceDimension = _VectorDimension>
   struct SystemModel {
-    enum { VectorDimension = _VectorDimension };
-    enum { CovarianceDimension = _CovarianceDimension };
+    static const int VectorDimension = _VectorDimension;
+    static const int CovarianceDimension = _CovarianceDimension;
 
     typedef State::Vector StateVector;
     typedef State::Covariance NoiseVariance;
     typedef State::SystemMatrix SystemMatrix;
 
-    enum { InputDimension = traits::Input<Derived>::Dimension };
+    static const int InputDimension = traits::Input<Derived>::Dimension;
     typedef typename traits::Input<Derived>::Type   InputType;
     typedef typename traits::Input<Derived>::Vector InputVector;
-    typedef Matrix_<State::Covariance::RowsAtCompileTime,InputDimension> InputMatrix;
+    typedef typename Matrix_<State::Covariance::RowsAtCompileTime,InputDimension>::type InputMatrix;
 
     typedef SubState_<VectorDimension,CovarianceDimension> SubState;
-    typedef ColumnVector_<VectorDimension> Vector;
+    typedef typename ColumnVector_<VectorDimension>::type Vector;
 
     typedef typename SubState::VectorSegment VectorSegment;
     typedef typename SubState::CovarianceBlock CovarianceBlock;
@@ -88,14 +88,14 @@ namespace traits {
   #define SYSTEM_MODEL_TRAIT(Derived, _VectorDimension, _CovarianceDimension) \
     typedef traits::SystemModel<Derived, _VectorDimension, _CovarianceDimension> trait; \
     \
-    enum { VectorDimension = trait::VectorDimension }; \
-    enum { CovarianceDimension = trait::CovarianceDimension }; \
+    static const int VectorDimension = trait::VectorDimension; \
+    static const int CovarianceDimension = trait::CovarianceDimension; \
     \
     typedef typename trait::StateVector   StateVector; \
     typedef typename trait::NoiseVariance NoiseVariance; \
     typedef typename trait::SystemMatrix  SystemMatrix; \
     \
-    enum { InputDimension = trait::InputDimension }; \
+    static const int InputDimension = trait::InputDimension; \
     typedef typename trait::InputType     InputType; \
     typedef typename trait::InputVector   InputVector; \
     typedef typename trait::InputMatrix   InputMatrix; \

hector_pose_estimation_core/include/hector_pose_estimation/system_model.inl
@@ -101,7 +101,7 @@ struct TimeContinuousSystemModel_<ConcreteModel, _VectorDimension, _CovarianceDi
     : x_diff(state.getVectorDimension())
     , A(state.getCovarianceDimension(), state.getCovarianceDimension())
     , B(state.getCovarianceDimension(), InputDimension)
-    , Q(state.getCovarianceDimension())
+    , Q(state.getCovarianceDimension(), state.getCovarianceDimension())
   {}
 };
 
@@ -145,9 +145,8 @@ void TimeContinuousSystemModel_<ConcreteModel, _VectorDimension, _CovarianceDime
   if (!internal_) internal_ = new internal(state);
   //if (init) internal_->Q = NoiseVariance::Zero(Q.rows(), Q.cols());
   getSystemNoise(internal_->Q, state, init);
-  // Q = dt * internal_->Q;
-  Q = internal_->Q; // use SymmetricMatrix assignment operator
-  Q *= dt;
+  Q = dt * internal_->Q;
+  Q.symmetric();
 }
 
 } // namespace hector_pose_estimation

hector_pose_estimation_core/include/hector_pose_estimation/Eigen/MatrixBaseAddons.h
@@ -1,5 +1,5 @@
 //=================================================================================================
-// Copyright (c) 2013, Johannes Meyer and contributors, Technische Universitat Darmstadt
+// Copyright (c) 2014, Johannes Meyer and contributors, Technische Universitat Darmstadt
 // All rights reserved.
 
 // Redistribution and use in source and binary forms, with or without
@@ -26,51 +26,45 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //=================================================================================================
 
+#ifndef HECTOR_POSE_ESTIMATION_EIGEN_MATRIXBASE_PLUGIN
+#define HECTOR_POSE_ESTIMATION_EIGEN_MATRIXBASE_PLUGIN
 
-#ifndef HECTOR_POSE_ESTIMATION_GROUND_VEHICLE_MODEL_H
-#define HECTOR_POSE_ESTIMATION_GROUND_VEHICLE_MODEL_H
+  #ifndef EIGEN_MATRIXBASE_PLUGIN
+    #define EIGEN_MATRIXBASE_PLUGIN <hector_pose_estimation/Eigen/MatrixBaseAddons.h>
+  #endif
 
-#include <hector_pose_estimation/system/generic_quaternion_system_model.h>
+#else
 
-namespace hector_pose_estimation {
+  Derived& setSymmetric() {
+    EIGEN_STATIC_ASSERT((RowsAtCompileTime == ColsAtCompileTime) ||
+                        (RowsAtCompileTime == Dynamic) ||
+                        (ColsAtCompileTime == Dynamic),
+                        THIS_METHOD_IS_ONLY_FOR_MATRICES_OF_A_SPECIFIC_SIZE);
+    *this = (*this + this->transpose()) / Scalar(2);
+    return derived();
+  }
 
-class GroundVehicleModel;
+  Derived& assertSymmetric() {
+    EIGEN_STATIC_ASSERT((RowsAtCompileTime == ColsAtCompileTime) ||
+                        (RowsAtCompileTime == Dynamic) ||
+                        (ColsAtCompileTime == Dynamic),
+                        THIS_METHOD_IS_ONLY_FOR_MATRICES_OF_A_SPECIFIC_SIZE);
+#if defined(ASSERT_SYMMETRIC_MATRIX_TO_BE_SYMMETRIC)
+    eigen_assert(this->isApprox(this->transpose(), ASSERT_SYMMETRIC_MATRIX_TO_BE_SYMMETRIC_PRECISION));
+#endif
+#if defined(FORCE_SYMMETRIC_MATRIX_TO_BE_SYMMETRIC)
+    return setSymmetric();
+#else
+    return derived();
+#endif
+  }
 
-//namespace traits {
-//  template <> struct Input<GroundVehicleModel> {
-//    enum { Dimension = ImuInput::Dimension };
-//    typedef ImuInput Type;
-//    typedef ImuInput::Vector Vector;
-//    typedef ImuInput::Variance Variance;
-//  };
-//} // namespace traits
+  Derived symmetric() const {
+    EIGEN_STATIC_ASSERT((RowsAtCompileTime == ColsAtCompileTime) ||
+                        (RowsAtCompileTime == Dynamic) ||
+                        (ColsAtCompileTime == Dynamic),
+                        THIS_METHOD_IS_ONLY_FOR_MATRICES_OF_A_SPECIFIC_SIZE);
+    return (*this + this->transpose()) / Scalar(2);
+  }
 
-class GroundVehicleModel : public GenericQuaternionSystemModel
-{
-public:
-  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-
-  GroundVehicleModel();
-  virtual ~GroundVehicleModel();
-
-  virtual void getPrior(State &state);
-  virtual SystemStatus getStatusFlags(const State& state);
-
-  using GenericQuaternionSystemModel::getDerivative;
-  virtual void getDerivative(StateVector& x_dot, const State& state);
-  using GenericQuaternionSystemModel::getStateJacobian;
-  virtual void getStateJacobian(SystemMatrix& A, const State& state, bool init = true);
-
-  virtual bool limitState(State& state);
-
-protected:
-  double gain_;
-  double base_height_, min_height_, max_height_;
-  typename Matrix_<3,3>::type dR3;
-};
-
-extern template class System_<GenericQuaternionSystemModel>;
-
-} // namespace hector_pose_estimation
-
-#endif // HECTOR_POSE_ESTIMATION_GROUND_VEHICLE_MODEL_H
+#endif // HECTOR_POSE_ESTIMATION_EIGEN_MATRIXBASE_PLUGIN

hector_pose_estimation_core/include/hector_pose_estimation/filter/ekf.h
@@ -57,8 +57,8 @@ public:
     Predictor(EKF *filter)
       : filter_(filter)
       , x_diff(filter->state().getVectorDimension())
-      , A(filter->state().getCovarianceDimension(),filter->state().getCovarianceDimension())
-      , Q(filter->state().getCovarianceDimension())
+      , A(filter->state().getCovarianceDimension(), filter->state().getCovarianceDimension())
+      , Q(filter->state().getCovarianceDimension(), filter->state().getCovarianceDimension())
     {}
     virtual ~Predictor() {}
     virtual bool predict(double dt) = 0;
@@ -116,7 +116,7 @@ public:
       , error(model->getDimension())
       , C(model->getDimension(), filter->state().getCovarianceDimension())
       , CP(model->getDimension(), filter->state().getCovarianceDimension())
-      , S(model->getDimension())
+      , S(model->getDimension(), model->getDimension())
       , K(filter->state().getCovarianceDimension(), model->getDimension())
       , update(filter->state().getCovarianceDimension())
     {}
@@ -129,7 +129,7 @@ public:
     typename Model::MeasurementVector y_pred;
     typename Model::MeasurementVector error;
     typename Model::MeasurementMatrix C;
-    Matrix_<ConcreteModel::MeasurementDimension, Dynamic> CP;
+    typename Matrix_<ConcreteModel::MeasurementDimension, Dynamic>::type CP;
     typename Model::NoiseVariance S;
     typename Model::GainMatrix K;
     typename Model::UpdateVector update;

hector_pose_estimation_core/include/hector_pose_estimation/filter/ekf.inl
@@ -48,6 +48,7 @@ bool EKF::Predictor_<ConcreteModel, Enabled>::predict(double dt) {
   ROS_DEBUG_STREAM_NAMED("ekf.prediction", "dt * A    = [" << std::endl << A << "]");
 
 //  state().P0() = A * state().P() * A.transpose() + Q;
+//  state().P0().assertSymmetric();
 //  state().x0() = x_pred;
 
   this->init_ = false;
@@ -71,6 +72,7 @@ bool EKF::Corrector_<ConcreteModel, Enabled>::correct(const typename ConcreteMod
   S.noalias()  = CP * C.transpose() + R;
   K.noalias() = CP.transpose() * S.inverse();
   state().P().noalias() -= K * CP;
+  state().P().assertSymmetric();
 
   error.noalias() = y - y_pred;
   this->model_->limitError(error);

hector_pose_estimation_core/include/hector_pose_estimation/matrix.h
@@ -30,6 +30,7 @@
 #define HECTOR_POSE_ESTIMATION_MATRIX_H
 
 #include <hector_pose_estimation/matrix_config.h>
+#include <hector_pose_estimation/Eigen/MatrixBaseAddons.h>
 #include <hector_pose_estimation/Eigen/QuaternionBaseAddons.h>
 
 #include <Eigen/Core>
@@ -48,163 +49,53 @@ namespace hector_pose_estimation {
   typedef Eigen::Quaternion<ScalarType> Quaternion;
 
   template <int Rows>
-  class ColumnVector_ : public Eigen::Matrix<ScalarType,Rows,1,0,(Rows != Dynamic ? Rows : MaxVectorSize),1> {
-  public:
-    typedef ColumnVector_<Rows> Derived;
-    typedef Eigen::Matrix<ScalarType,Rows,1,0,(Rows != Dynamic ? Rows : MaxVectorSize),1> Base;
-    typedef Eigen::Map<Base> Map;
-    typedef Eigen::Map<const Base> ConstMap;
-
-    ColumnVector_() { this->setZero(); }
-    explicit ColumnVector_(IndexType size) : Base(size) { assert(Rows == Dynamic || size == Rows); this->setZero(); }
-//    explicit ColumnVector_(ScalarType value) { this->setConstant(value); }
-    ColumnVector_(ScalarType x, ScalarType y, ScalarType z) : Base(x, y, z) {}
-    template <typename OtherDerived> ColumnVector_(const Eigen::MatrixBase<OtherDerived>& other) : Base(other) {}
-
-    EIGEN_INHERIT_ASSIGNMENT_EQUAL_OPERATOR(Derived)
-    Derived& operator=(ScalarType scalar) { this->setConstant(scalar); return *this; }
+  struct ColumnVector_ {
+    typedef Eigen::Matrix<ScalarType, Rows, 1,
+                          Eigen::ColMajor,
+                          (Rows != Dynamic ? Rows : MaxVectorSize), 1
+                         > type;
   };
-  typedef ColumnVector_<Dynamic> ColumnVector;
-  typedef ColumnVector_<3> ColumnVector3;
+  typedef typename ColumnVector_<Dynamic>::type ColumnVector;
+  typedef typename ColumnVector_<3>::type ColumnVector3;
 
   template <int Cols>
-  class RowVector_ : public Eigen::Matrix<ScalarType,1,Cols,Eigen::RowMajor,1,MaxVectorSize> {
-  public:
-    typedef RowVector_<Cols> Derived;
-    typedef Eigen::Matrix<ScalarType,1,Cols,0,1,MaxVectorSize> Base;
-    typedef Eigen::Map<Base> Map;
-    typedef Eigen::Map<const Base> ConstMap;
-
-    RowVector_() { this->setZero(); }
-    explicit RowVector_(IndexType size) : Base(size) { assert(Cols == Dynamic || size == Cols); this->setZero(); }
-//    explicit RowVector_(ScalarType value) { this->setConstant(value); }
-    RowVector_(ScalarType x, ScalarType y, ScalarType z) : Base(x, y, z) {}
-    template <typename OtherDerived> RowVector_(const Eigen::MatrixBase<OtherDerived>& other) : Base(other) {}
-
-    EIGEN_INHERIT_ASSIGNMENT_EQUAL_OPERATOR(Derived)
-    Derived& operator=(ScalarType scalar) { this->setConstant(scalar); return *this; }
+  struct RowVector_ {
+      typedef Eigen::Matrix<ScalarType, 1, Cols,
+                            Eigen::RowMajor,
+                            1, (Cols != Dynamic ? Cols : MaxVectorSize)
+                           > type;
   };
-  typedef RowVector_<Dynamic> RowVector;
-  typedef RowVector_<3> RowVector3;
+  typedef typename RowVector_<Dynamic>::type RowVector;
+  typedef typename RowVector_<3>::type RowVector3;
 
   template <int Rows, int Cols>
-  class Matrix_ : public Eigen::Matrix<ScalarType,Rows,Cols,(Rows==1 && Cols!=1 ? Eigen::RowMajor : Eigen::ColMajor),(Rows != Dynamic ? Rows : MaxMatrixRowsCols),(Cols != Dynamic ? Cols : MaxMatrixRowsCols)> {
-  public:
-    typedef Matrix_<Rows,Cols> Derived;
-    typedef Eigen::Matrix<ScalarType,Rows,Cols,(Rows==1 && Cols!=1 ? Eigen::RowMajor : Eigen::ColMajor),(Rows != Dynamic ? Rows : MaxMatrixRowsCols),(Cols != Dynamic ? Cols : MaxMatrixRowsCols)> Base;
-    typedef Eigen::Map<Base> Map;
-    typedef Eigen::Map<const Base> ConstMap;
-
-    Matrix_() { this->setZero(); }
-    explicit Matrix_(IndexType rows, IndexType cols) : Base(rows, cols) { assert(Rows == Dynamic || rows == Rows); assert(Cols == Dynamic || cols == Cols); this->setZero(); }
-//    explicit Matrix_(ScalarType value) { this->setConstant(value); }
-    template <typename OtherDerived> Matrix_(const Eigen::MatrixBase<OtherDerived>& other) : Base(other) {}
-
-    EIGEN_INHERIT_ASSIGNMENT_EQUAL_OPERATOR(Derived)
-    Derived& operator=(ScalarType scalar) { this->setConstant(scalar); return *this; }
+  struct Matrix_ {
+      typedef Eigen::Matrix<ScalarType, Rows, Cols,
+                            (Rows == 1 && Cols != 1 ? Eigen::RowMajor : Eigen::ColMajor),
+                            (Rows != Dynamic ? Rows : MaxMatrixRowsCols),
+                            (Cols != Dynamic ? Cols : MaxMatrixRowsCols)
+                           > type;
   };
-  typedef Matrix_<Dynamic,Dynamic> Matrix;
-  typedef Matrix_<3,3> Matrix3;
+  typedef typename Matrix_<Dynamic,Dynamic>::type Matrix;
+  typedef typename Matrix_<3,3>::type Matrix3;
 
   template <int RowsCols>
-  class SymmetricMatrix_ : public Matrix_<RowsCols,RowsCols> {
-  public:
-    typedef SymmetricMatrix_<RowsCols> Derived;
-    typedef Matrix_<RowsCols,RowsCols> Storage;
-    using typename Storage::Base;
-//    typedef Eigen::SelfAdjointView<typename Storage::Base,Upper> SelfAdjointView;
-    using typename Storage::Map;
-    using typename Storage::ConstMap;
-
-    // Constructors
-    SymmetricMatrix_() {}
-    //explicit SymmetricMatrix_(ScalarType value) { this->setConstant(value); }
-    explicit SymmetricMatrix_(IndexType dim) : Storage(dim, dim) { assert(RowsCols == Dynamic || dim == RowsCols); }
-    template <typename OtherDerived> SymmetricMatrix_(const Eigen::MatrixBase<OtherDerived>& other) : Storage(other) {
-#if defined(FORCE_SYMMETRIC_MATRIX_TO_BE_SYMMETRIC)
-      symmetric();
-#endif
-    }
-    SymmetricMatrix_(const Derived& other) : Storage(other) {}
-
-    template <typename OtherDerived> Derived& operator=(const Eigen::MatrixBase<OtherDerived>& other) {
-      this->Storage::operator=(other);
-#if defined(ASSERT_SYMMETRIC_MATRIX_TO_BE_SYMMETRIC)
-      eigen_assert(this->isApprox(this->transpose(), ASSERT_SYMMETRIC_MATRIX_TO_BE_SYMMETRIC_PRECISION));
-#endif
-#if defined(FORCE_SYMMETRIC_MATRIX_TO_BE_SYMMETRIC)
-      return symmetric();
-#else
-      return *this;
-#endif
-    }
-
-    Derived& operator=(const Derived& other) {
-      this->Storage::operator=(other);
-      return *this;
-    }
-
-    Derived& symmetric() {
-      this->Storage::operator=((*this + this->transpose()) / 2);
-      return *this;
-    }
-
-//    template <typename OtherDerived> SymmetricMatrix_<Derived::RowsAtCompileTime> quadratic(const Eigen::MatrixBase<OtherDerived>& other) {
-//      return other * this->template selfadjointView<Upper>() * other.transpose();
-//    }
-
-//    Derived inverse() const {
-////      return this->template selfadjointView<Upper>().inverse();
-//      return this->Storage::inverse().eval();
-//    }
+  struct SymmetricMatrix_ {
+      typedef typename Matrix_<RowsCols,RowsCols>::type type;
   };
-  typedef SymmetricMatrix_<3> SymmetricMatrix3;
+  typedef typename SymmetricMatrix_<3>::type SymmetricMatrix3;
+  typedef typename SymmetricMatrix_<6>::type SymmetricMatrix6;
+  typedef typename SymmetricMatrix_<Dynamic>::type SymmetricMatrix;
 
-  class SymmetricMatrix : public SymmetricMatrix_<Dynamic> {
-  public:
-    typedef SymmetricMatrix Derived;
-    typedef SymmetricMatrix_<Dynamic> Storage;
-    using typename Storage::Base;
-//    typedef Eigen::SelfAdjointView<typename Storage::Base,Upper> SelfAdjointView;
-    using typename Storage::Map;
-    using typename Storage::ConstMap;
-
-    // Constructors
-    SymmetricMatrix() : Storage() {}
-    SymmetricMatrix(IndexType dim) : Storage(dim) {}
-    // SymmetricMatrix(IndexType dim, ScalarType value) : Storage(dim) { this->setConstant(value); }
-    template <int OtherRowsCols> SymmetricMatrix(const SymmetricMatrix_<OtherRowsCols>& other) : Storage(other) {}
-    template <typename OtherDerived> SymmetricMatrix(const Eigen::MatrixBase<OtherDerived>& other) : Storage(other) {}
-
-    template <typename OtherDerived> Derived& operator=(const Eigen::MatrixBase<OtherDerived>& other) {
-      this->Storage::operator=(other);
-      return symmetric();
-    }
-
-    Derived& symmetric() {
-      this->Storage::symmetric();
-      return *this;
-    }
-
-    void resize(IndexType size) {
-      this->Storage::resize(size, size);
-    }
-
-    void conservativeResize(IndexType size) {
-      this->Storage::conservativeResize(size, size);
-    }
-  };
-
-  class SkewSymmetricMatrix : public Matrix3 {
-  public:
-    template <typename OtherDerived> SkewSymmetricMatrix(const Eigen::MatrixBase<OtherDerived>& other) : Matrix3()
-    {
-      EIGEN_STATIC_ASSERT_VECTOR_SPECIFIC_SIZE(Eigen::MatrixBase<OtherDerived>, 3);
-      *this <<  0.0,       -other.z(),  other.y(),
-                other.z(),  0.0,       -other.x(),
-               -other.y(),  other.x(),  0.0;
-    }
-  };
+  template <typename OtherDerived>
+  static inline Matrix3 SkewSymmetricMatrix(const Eigen::MatrixBase<OtherDerived>& other) {
+    EIGEN_STATIC_ASSERT_VECTOR_SPECIFIC_SIZE(Eigen::MatrixBase<OtherDerived>, 3);
+    Matrix3 result;
+    result <<  0.0,       -other.z(),  other.y(),
+               other.z(),  0.0,       -other.x(),
+              -other.y(),  other.x(),  0.0;
+    return result;
+  }
 
 } // namespace hector_pose_estimation
 
@@ -226,51 +117,6 @@ namespace Eigen {
   : public DenseStorage<T, 0, 0, 0, _Options> { };
 #endif
 
-namespace internal {
-
-#define EIGEN_FORWARD_TRAITS_FOR_TYPE(Base) \
-  typedef typename traits<Base>::Scalar Scalar; \
-  typedef typename traits<Base>::StorageKind StorageKind; \
-  typedef typename traits<Base>::Index Index; \
-  typedef typename traits<Base>::XprKind XprKind; \
-  enum { \
-    RowsAtCompileTime = traits<Base>::RowsAtCompileTime, \
-    ColsAtCompileTime = traits<Base>::ColsAtCompileTime, \
-    MaxRowsAtCompileTime = traits<Base>::MaxRowsAtCompileTime, \
-    MaxColsAtCompileTime = traits<Base>::MaxColsAtCompileTime, \
-    Flags = traits<Base>::Flags, \
-    CoeffReadCost = traits<Base>::CoeffReadCost, \
-    Options = traits<Base>::Options, \
-    InnerStrideAtCompileTime = traits<Base>::InnerStrideAtCompileTime, \
-    OuterStrideAtCompileTime = traits<Base>::OuterStrideAtCompileTime \
-  }
-
-template <int Rows> struct traits< hector_pose_estimation::ColumnVector_<Rows> > {
-  typedef typename hector_pose_estimation::ColumnVector_<Rows>::Base EigenType;
-  EIGEN_FORWARD_TRAITS_FOR_TYPE(EigenType);
-};
-
-template <int Cols> struct traits< hector_pose_estimation::RowVector_<Cols> > {
-  typedef typename hector_pose_estimation::RowVector_<Cols>::Base EigenType;
-  EIGEN_FORWARD_TRAITS_FOR_TYPE(EigenType);
-};
-
-template <int Rows, int Cols> struct traits< hector_pose_estimation::Matrix_<Rows,Cols> > {
-  typedef typename hector_pose_estimation::Matrix_<Rows,Cols>::Base EigenType;
-  EIGEN_FORWARD_TRAITS_FOR_TYPE(EigenType);
-};
-
-template <int RowsCols> struct traits< hector_pose_estimation::SymmetricMatrix_<RowsCols> > {
-  typedef typename hector_pose_estimation::SymmetricMatrix_<RowsCols>::Base EigenType;
-  EIGEN_FORWARD_TRAITS_FOR_TYPE(EigenType);
-};
-
-template <> struct traits< hector_pose_estimation::SymmetricMatrix > {
-  typedef hector_pose_estimation::SymmetricMatrix::Base EigenType;
-  EIGEN_FORWARD_TRAITS_FOR_TYPE(EigenType);
-};
-
-} // namespace internal
 } // namespace Eigen
 
 namespace hector_pose_estimation {

commit c23ebe2a7e2831355ea30409522a2b898b692cb5

hector_pose_estimation_core: reverted integer constants in SystemModel and MeasurementModel classes to enum to avoid linker problems
This fixes a regression from 81b976da6a54197357c2fa083d9c4e20e9121019.

hector_pose_estimation_core/include/hector_pose_estimation/input.h
@@ -109,7 +109,7 @@ protected:
 
 namespace traits {
   template <class Model> struct Input {
-    static const int Dimension = 0;
+    enum { Dimension = 0 };
     typedef Input_<Dimension> Type;
     typedef ColumnVector_<0> Vector;
     typedef SymmetricMatrix_<0> Variance;

hector_pose_estimation_core/include/hector_pose_estimation/measurement_model.h
@@ -55,14 +55,14 @@ namespace traits {
 
   template <class Derived, int _Dimension = Derived::MeasurementDimension>
   struct MeasurementModel {
-    static const int MeasurementDimension = _Dimension;
+    enum { MeasurementDimension = _Dimension };
     typedef typename ColumnVector_<MeasurementDimension>::type MeasurementVector;
     typedef typename SymmetricMatrix_<MeasurementDimension>::type NoiseVariance;
     typedef typename Matrix_<MeasurementDimension,Dynamic>::type MeasurementMatrix;
     typedef typename Matrix_<State::Covariance::RowsAtCompileTime,MeasurementDimension>::type GainMatrix;
     typedef typename ColumnVector_<State::Covariance::RowsAtCompileTime>::type UpdateVector;
 
-    static const int InputDimension = traits::Input<Derived>::Dimension;
+    enum { InputDimension = traits::Input<Derived>::Dimension };
     typedef typename traits::Input<Derived>::Type InputType;
     typedef typename traits::Input<Derived>::Vector InputVector;
     typedef typename Matrix_<MeasurementDimension,InputDimension>::type InputMatrix;
@@ -71,14 +71,14 @@ namespace traits {
   #define MEASUREMENT_MODEL_TRAIT(Derived, _Dimension) \
     typedef typename traits::MeasurementModel<Derived, _Dimension> trait; \
     \
-    static const int MeasurementDimension = _Dimension; \
+    enum { MeasurementDimension = _Dimension }; \
     typedef typename trait::MeasurementVector MeasurementVector; \
     typedef typename trait::NoiseVariance NoiseVariance; \
     typedef typename trait::MeasurementMatrix MeasurementMatrix; \
     typedef typename trait::GainMatrix GainMatrix; \
     typedef typename trait::UpdateVector UpdateVector; \
     \
-    static const int InputDimension = trait::InputDimension; \
+    enum { InputDimension = trait::InputDimension }; \
     typedef typename trait::InputType InputType; \
     typedef typename trait::InputVector InputVector; \
     typedef typename trait::InputMatrix InputMatrix; \

hector_pose_estimation_core/include/hector_pose_estimation/system_model.h
@@ -59,14 +59,14 @@ namespace traits {
 
   template <class Derived, int _VectorDimension = Derived::VectorDimension, int _CovarianceDimension = _VectorDimension>
   struct SystemModel {
-    static const int VectorDimension = _VectorDimension;
-    static const int CovarianceDimension = _CovarianceDimension;
+    enum { VectorDimension = _VectorDimension };
+    enum { CovarianceDimension = _CovarianceDimension };
 
     typedef State::Vector StateVector;
     typedef State::Covariance NoiseVariance;
     typedef State::SystemMatrix SystemMatrix;
 
-    static const int InputDimension = traits::Input<Derived>::Dimension;
+    enum { InputDimension = traits::Input<Derived>::Dimension };
     typedef typename traits::Input<Derived>::Type   InputType;
     typedef typename traits::Input<Derived>::Vector InputVector;
     typedef typename Matrix_<State::Covariance::RowsAtCompileTime,InputDimension>::type InputMatrix;
@@ -88,14 +88,14 @@ namespace traits {
   #define SYSTEM_MODEL_TRAIT(Derived, _VectorDimension, _CovarianceDimension) \
     typedef traits::SystemModel<Derived, _VectorDimension, _CovarianceDimension> trait; \
     \
-    static const int VectorDimension = trait::VectorDimension; \
-    static const int CovarianceDimension = trait::CovarianceDimension; \
+    enum { VectorDimension = trait::VectorDimension }; \
+    enum { CovarianceDimension = trait::CovarianceDimension }; \
     \
     typedef typename trait::StateVector   StateVector; \
     typedef typename trait::NoiseVariance NoiseVariance; \
     typedef typename trait::SystemMatrix  SystemMatrix; \
     \
-    static const int InputDimension = trait::InputDimension; \
+    enum { InputDimension = trait::InputDimension }; \
     typedef typename trait::InputType     InputType; \
     typedef typename trait::InputVector   InputVector; \
     typedef typename trait::InputMatrix   InputMatrix; \

hector_pose_estimation_core/include/hector_pose_estimation/system_model.inl
@@ -100,7 +100,7 @@ struct TimeContinuousSystemModel_<ConcreteModel, _VectorDimension, _CovarianceDi
   internal(const State &state)
     : x_diff(state.getVectorDimension())
     , A(state.getCovarianceDimension(), state.getCovarianceDimension())
-    , B(state.getCovarianceDimension(), InputDimension)
+    , B(state.getCovarianceDimension(), IndexType(InputDimension))
     , Q(state.getCovarianceDimension(), state.getCovarianceDimension())
   {}
 };